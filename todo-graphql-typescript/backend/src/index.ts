75e77bb09182574e109b50389af156d3b0a9f51a
Paolo Tiu <paolotiu17@gmail.com>
2021-03-18 23:18:01 +0800

    graphql path

--- todo-graphql-typescript/backend/src/index.ts
+++ todo-graphql-typescript/backend/src/index.ts
@@ -1,16 +1,14 @@
 import { resolvers } from "./resolvers";
 import { ApolloServer, gql } from "apollo-server-express";
 import { typeDefs } from "./schema/schema";
-import logger from "morgan";
 import express from "express";
 
 const app = express();
-console.log(typeDefs.definitions.map((x) => (x as any).name));
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
-server.applyMiddleware({ app, path: "/Someother" });
+server.applyMiddleware({ app, path: "/graphql" });
 
 app.listen(4000, () => {
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
