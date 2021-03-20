11286b869a61ddc47300c910439ed10c252edb83
Paolo Tiu <paolotiu17@gmail.com>
2021-03-20 21:19:45 +0800

    added a check if table already exists

--- todo-graphql-typescript/backend/src/db.ts
+++ todo-graphql-typescript/backend/src/db.ts
@@ -29,11 +29,11 @@ export const query = (
 // Initiate db if no tables and/or fields are made
 const initiateDb = async () => {
   await query(
-    "CREATE TABLE categories( id serial PRIMARY KEY,name text not null);"
+    "CREATE TABLE IF NOT EXISTS categories( id serial PRIMARY KEY,name text not null);"
   );
 
   await query(
-    "CREATE TABLE items(id serial PRIMARY KEY, name text not null, price int not null, category int references categories(id) not null);"
+    "CREATE TABLE IF NOT EXISTS items(id serial PRIMARY KEY, name text not null, price int not null, category int references categories(id) not null);"
   );
 };
 
