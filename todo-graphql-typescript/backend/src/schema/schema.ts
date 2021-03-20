c259d2b7b20ee585e6a9eeed5466efd7dff47c78
Paolo Tiu <paolotiu17@gmail.com>
2021-03-20 21:45:41 +0800

    updated schema

--- todo-graphql-typescript/backend/src/schema/schema.ts
+++ todo-graphql-typescript/backend/src/schema/schema.ts
@@ -2,12 +2,21 @@ import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
   type Query {
-    helloWorld: String!
-    hey: Role
+    allItems: [Item!]!
+    item(id: ID!): Item!
+    category(name: String!): Category!
+  }
+  type Category {
+    name: String!
+    items: [Item!]!
+  }
+  type Item {
+    id: ID!
+    name: String!
+    price: Int!
   }
 
-  enum Role {
-    USER
-    ADMIN
+  type Mutation {
+    createItem(name: String!, price: Int!): Item!
   }
 `;
