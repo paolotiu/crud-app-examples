67d7dce576fa0bf58ff2f9aebdf92143d66d5c93
Paolo Tiu <paolotiu17@gmail.com>
2021-03-21 19:27:47 +0800

    added compression

--- todo-graphql-typescript/backend/src/index.ts
+++ todo-graphql-typescript/backend/src/index.ts
@@ -3,6 +3,7 @@ import { ApolloServer } from "apollo-server-express";
 import { typeDefs } from "./schema/schema";
 import express from "express";
 import { connectDb } from "./db";
+import compression from "compression";
 
 const startServer = async () => {
   const app = express();
@@ -13,13 +14,14 @@ const startServer = async () => {
   });
 
   await connectDb();
+
+  app.use(compression());
+  server.applyMiddleware({ app, path: "/graphql" });
   app.listen(4000, () => {
     console.log(
       `🚀 Server ready at http://localhost:4000${server.graphqlPath}`
     );
   });
-
-  server.applyMiddleware({ app, path: "/graphql" });
 };
 
 startServer();
