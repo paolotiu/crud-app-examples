ab45bf244f97e20faf9ece0a8450aeb16015f052
Paolo Tiu <paolotiu17@gmail.com>
2021-03-21 19:00:25 +0800

    added category queries

--- todo-graphql-typescript/backend/src/util/queries.ts
+++ todo-graphql-typescript/backend/src/util/queries.ts
@@ -2,6 +2,7 @@ import { Category, Item } from "./../generated/graphql";
 import { query } from "../db";
 import { ItemAndCategoryIDs } from "../types/modelTypes";
 
+// Get the item by Id
 export const oneItemById = async (id: string) => {
   const queryResult = await query<Item>({
     text: `
@@ -13,6 +14,19 @@ export const oneItemById = async (id: string) => {
 
   return queryResult.rows[0];
 };
+
+// // Get item(s) by name
+// export const itemsByName = async (name: string) => {
+//   const queryResult = await query<Item>({
+//     text: `
+//             SELECT * FROM items
+//             WHERE name = $1;
+//         `,
+//     values: [name],
+//   });
+
+//   return queryResult.rows;
+// };
 
 export const oneCategoryById = async (id: string) => {
   const queryResult = await query<Category>({
@@ -36,4 +50,37 @@ export const addItemToCategory = async (itemId: string, categoryId: string) => {
   });
 
   return queryResult.rows[0];
+};
+
+export const allItemsFromCategory = async (categoryId: string) => {
+  const queryResult = await query<Item>(
+    {
+      text: `
+            SELECT * FROM items i 
+            WHERE i.id in (
+                SELECT ic.item_id FROM item_in_category ic
+                WHERE ic.category_id = $1
+            );
+        `,
+    },
+    [categoryId]
+  );
+  return queryResult.rows;
+};
+
+export const removeItemFromCategory = async (
+  itemId: string,
+  categoryId: string
+) => {
+  const queryResult = await query({
+    text: `
+        DELETE FROM item_in_category
+        WHERE item_id = $1 and category_id = $2;
+    `,
+    values: [itemId, categoryId],
+  });
+
+  // Return true if a row is affected
+  // False if not
+  return !!queryResult.rowCount;
 };
