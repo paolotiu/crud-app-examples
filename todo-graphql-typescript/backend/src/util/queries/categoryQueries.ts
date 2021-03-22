8bb3d55572bc74d0ac213e3db8d25ddac8309fc4
Paolo Tiu <paolotiu17@gmail.com>
2021-03-22 14:27:41 +0800

    modularized queries

--- todo-graphql-typescript/backend/src/util/queries/categoryQueries.ts
+++ todo-graphql-typescript/backend/src/util/queries/categoryQueries.ts
@@ -1,42 +1,6 @@
-import { Category, Item } from "./../generated/graphql";
-import { query } from "../db";
-import { ItemAndCategoryIDs } from "../types/modelTypes";
-
-// get all items
-export const allItems = async () => {
-  const queryResult = await query<Item>({
-    text: `
-    SELECT * FROM items;
-    `,
-  });
-  return queryResult.rows;
-};
-
-// Get the item by Id
-export const oneItemById = async (id: string) => {
-  const queryResult = await query<Item>({
-    text: `
-      SELECT * FROM items
-      WHERE id = $1
-      `,
-    values: [id],
-  });
-
-  return queryResult.rows[0];
-};
-
-// Get item(s) by name
-export const getItemsByName = async (name: string) => {
-  const queryResult = await query<Item>({
-    text: `
-            SELECT * FROM items
-            WHERE name = $1;
-        `,
-    values: [name],
-  });
-
-  return queryResult.rows;
-};
+import { Category, Item } from "./../../generated/graphql";
+import { query } from "../../db";
+import { ItemAndCategoryIDs } from "../../types/modelTypes";
 
 // get category by Id
 export const oneCategoryById = async (id: string) => {
