4c0da39bf4e6e8c87e8781577aacacf24113a6ab
Paolo Tiu <paolotiu17@gmail.com>
2021-03-23 17:20:46 +0800

    removed boilerplate

--- todo-graphql-typescript/backend/src/tests/graphqlTestCall.ts
+++ todo-graphql-typescript/backend/src/tests/graphqlTestCall.ts
@@ -6,25 +6,6 @@ import { resolvers } from "../resolvers";
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-export const graphqlTestCall = async (
-  query: any,
-  variables?: any,
-  userId?: number | string
-) => {
-  return graphql(
-    schema,
-    query,
-    undefined,
-    {
-      req: {
-        session: {
-          userId,
-        },
-      },
-      res: {
-        clearCookie: () => {},
-      },
-    },
-    variables
-  );
+export const graphqlTestCall = async (query: any, variables?: any) => {
+  return graphql(schema, query, undefined, variables);
 };
