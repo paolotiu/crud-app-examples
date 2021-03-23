55f3e9dc1481f2580ff011e178313a0bc6a9d529
Paolo Tiu <paolotiu17@gmail.com>
2021-03-23 14:27:04 +0800

    category name unique and item now had no cat row

--- todo-graphql-typescript/backend/src/db.ts
+++ todo-graphql-typescript/backend/src/db.ts
@@ -25,11 +25,11 @@ export const query = <ReturnType>(
 // Initiate db if no tables and/or fields are made
 const initiateDb = async () => {
   await query(
-    "CREATE TABLE IF NOT EXISTS categories( id serial PRIMARY KEY,name text not null);"
+    "CREATE TABLE IF NOT EXISTS categories( id serial PRIMARY KEY,name text not null unique);"
   );
 
   await query(
-    "CREATE TABLE IF NOT EXISTS items(id serial PRIMARY KEY, name text not null, price int not null, category int references categories(id) not null);"
+    "CREATE TABLE IF NOT EXISTS items(id serial PRIMARY KEY, name text not null, price int not null );"
   );
 
   await query(
